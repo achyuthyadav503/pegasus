320f350b0bff96806c270064eee33c53c3708234
achyuthyadav503 <36585243+achyuthyadav503@users.noreply.github.com>
2018-02-18 23:30:24 +0530

    regtotal
    regtotal

---
+++ myproject/src/app/users/users.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {Http,Response,Headers}from '@angular/http';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  constructor(private http:Http) { }
+  conformationString:string="New User has been added";
+  isAdded:boolean=false;
+  userObj:object=[];
+  //emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  emailPattern ="^\\w+([\\.-]?\\w+)*@\\w+([\.-]?\\w+)*(\\.\\w{2,3})+$";
+  textpattern="^[a-zA-Z\\s]+$"; 
+
+  ngOnInit() {
+  }
+  addNewUser=function (user) {
+    this.userObj={
+      "FirstName": user.FirstName,
+      "LastName": user.LastName,
+      "Qualification": user.Qualification,
+      "Occupation": user.Occupation,
+      "Industry": user.Industry,
+      "Firm": user.Firm,
+      "Workinglocation": user.Workinglocation,
+      "PersonalEmailID": user.PersonalEmailID,
+      "OfficialEmailID": user.OfficialEmailID
+    }
+    this.http.post("http://localhost:5555/users",this.userObj).
+    subscribe((res:Response)=>{
+     this.isAdded=true;
+    })
+  }
+
+
+}
